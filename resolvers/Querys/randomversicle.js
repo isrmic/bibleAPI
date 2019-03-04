4bba2dd5eaaa3d8ea047b577d141261bf0646458
isrmic <israelfcfm@outlook.com>
2019-03-04 16:27:36 -0300

    modified random versicle resolver

--- resolvers/Querys/randomversicle.js
+++ resolvers/Querys/randomversicle.js
@@ -1,23 +1,42 @@
-const randomversicle = () => async (root, args, { bibles }) => {
+const { generateRegexString } = require('./modules/generators');
+
+const randomversicle = () => async (root, {bookname, chapter}, { bibles }) => {
 
 	const nvt = bibles.version.nvt;
+	let books;
 
-	const randombook = Math.floor((Math.random() * 66 - 1));
-	const book = nvt.books[randombook];
+	if (bookname) {
+		const filter = new RegExp(generateRegexString(bookname), 'i');
+		books = nvt.books.filter(({ bookname }) => (filter.lastIndex = 0, filter.test(bookname)));
+	}
+	if (books.length === 0)
+		return {
+			bookname: '',
+			chapternumber: 0,
+			versiclenumber: 0,
+			content: 'Livro não encontrado',
+		};		
 
-	const randomchapter = Math.floor((Math.random() * book.chaptersnumber));
-	const chapter = book.chapters[randomchapter];
+	const randombook = Math.floor((Math.random() * (books.length)));
+	var book = books[randombook];
+
+	if (chapter) {
+		var chapter = book.chapters[chapter];
+	}
+	else {
+		var randomchapter = Math.floor((Math.random() * book.chaptersnumber));
+		var chapter = book.chapters[randomchapter];
+	}
 
 	const randomversicle = Math.floor((Math.random() * chapter.versiclesnumber));
 	const versiclecontent = chapter.versicles[randomversicle];
-
+	
 	return {
 		bookname: book.bookname,
 		chapternumber: randomchapter + 1,
 		versiclenumber: randomversicle + 1,
 		content: versiclecontent,
 	};
-
 };
 
 module.exports = randomversicle;
