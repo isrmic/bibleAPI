cba3c624ce13c6306423d62713d36c190fc1b61a
isrmic <israelfcfm@outlook.com>
2019-03-06 22:07:58 -0300

    modified load schemas | before: relative path; after: absolute path

--- schemas/index.js
+++ schemas/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-
-const Query = fs.readFileSync('./schemas/Query.gql', 'utf8');
-const Mutation = fs.readFileSync('./schemas/Mutation.gql', 'utf8');
+const { join } = require('path');
+const Query = fs.readFileSync(join(__dirname, '/Query.gql'), 'utf8');
+const Mutation = fs.readFileSync(join(__dirname, '/Mutation.gql'), 'utf8');
 
 module.exports = { Query, Mutation };
